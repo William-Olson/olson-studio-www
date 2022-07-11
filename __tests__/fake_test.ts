8e7937677348ae50f321cfd170d1773fd8f18cc4
William Olson <willko747@gmail.com>
2022-07-10 20:16:41 -0500

    fix: remove backend tests for now

--- __tests__/fake_test.ts
+++ __tests__/fake_test.ts
@@ -3,3 +3,12 @@ export {};
 test('some test should succeed', () => {
   expect(true).toBeTruthy();
 });
+
+test('should inject crypto as a global', () => {
+  expect(global.crypto).toBeDefined();
+
+  // test randomBytes
+  const res: string = (global.crypto as any).randomBytes(32).toString('hex');
+
+  expect(Buffer.byteLength(Buffer.from(res, 'hex'))).toBe(32);
+});
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testPathIgnorePatterns: ['.*setup[.]ts'],
+  // testMatch: ['.*spec[.]ts'],
+  // testEnvironment: 'jest-environment-node',
+  testEnvironment: 'node',
+  // testEnvironment: 'jsdom',
+  setupFiles: ['./__tests__/jest.setup.ts']
 };
